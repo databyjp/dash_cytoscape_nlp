0cdca5eb02d777d1847a9f2fabc8f152ebae3b90
JP Hwang <me@jphwang.com>
2020-05-15 08:18:32 +0100

    further revised init tsne settings

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ topics_txt = [[j.split("*")[1].replace('"', "") for j in i] for i in topics_txt]
 topics_txt = ["; ".join(i) for i in topics_txt]
 
 journal_ser = network_df.groupby("journal")["0"].count().sort_values(ascending=False)
-top_journals = list(journal_ser.index[:5])
+top_journals = list(journal_ser.index[:3])
 
 
 def tsne_to_cyto(tsne_val, scale_factor=40):
@@ -324,7 +324,7 @@ body_layout = dbc.Container(
                                         {"label": k, "value": k} for k in range(1, 21)
                                     ],
                                     clearable=False,
-                                    value=5,
+                                    value=3,
                                     style={"width": "50px"},
                                 )
                             ]
