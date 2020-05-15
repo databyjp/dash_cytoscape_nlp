a7a311e3d18f24faf5a5e19d0813235d3d45af79
JP Hwang <me@jphwang.com>
2020-05-15 09:03:21 +0100

    logger.DEBUG also reformatted with black

--- 1_lda_analysis.py
+++ 1_lda_analysis.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 root_logger = logging.getLogger()
 root_logger.setLevel(logging.DEBUG)
 sh = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 sh.setFormatter(formatter)
 root_logger.addHandler(sh)
 
@@ -26,32 +26,34 @@ from sklearn.manifold import TSNE
 nlp = spacy.load("en_core_web_sm")
 
 desired_width = 320
-pd.set_option('display.max_columns', 20)
-pd.set_option('display.width', desired_width)
+pd.set_option("display.max_columns", 20)
+pd.set_option("display.width", desired_width)
 
-if 'cord-nlp-cytoscape' in os.listdir('.'):
-    os.chdir('cord-nlp-cytoscape')
+if "cord-nlp-cytoscape" in os.listdir("."):
+    os.chdir("cord-nlp-cytoscape")
 
-datadir = 'comm_use_subset'
-data_files = [i for i in os.listdir(datadir) if '.json' in i]
+datadir = "comm_use_subset"
+data_files = [i for i in os.listdir(datadir) if ".json" in i]
 
 # For testing the script with a smaller dataset
 # data_files = data_files[:500]
 
 # ========== CONVERT STRINGS TO BoW ==========
-logger.info(f'Tokenising & Lemmatising {len(data_files)} files')
+logger.info(f"Tokenising & Lemmatising {len(data_files)} files")
 unigram_docs = []  # This will be the list of lemmatised abstracts
 unigram_datafiles = []
 docs_titles = []
 filenames = []
 counter = 0
 for datafile in data_files:
-    with open(os.path.join(datadir, datafile), 'r') as f:
+    with open(os.path.join(datadir, datafile), "r") as f:
         data_dict = json.load(f)
 
     # For using either the abstract data of the title data
-    abs_text = ' '.join([i['text'] for i in data_dict['abstract']])  # Abstract could be in multiple sections
-    title_text = data_dict['metadata']['title'].lower()
+    abs_text = " ".join(
+        [i["text"] for i in data_dict["abstract"]]
+    )  # Abstract could be in multiple sections
+    title_text = data_dict["metadata"]["title"].lower()
 
     # raw_text = title_text
     raw_text = abs_text
@@ -59,12 +61,15 @@ for datafile in data_files:
     # Tokenise & lemmatise data w/ spacy
     doc = nlp(raw_text)
     lemmas = [
-        token.lemma_.lower() for token in doc if (
-                not token.is_stop
-                and token.pos is not 'SYM'
-                and token.pos_ is not 'PUNCT'
-                and len(token) > 1
-        )]
+        token.lemma_.lower()
+        for token in doc
+        if (
+            not token.is_stop
+            and token.pos is not "SYM"
+            and token.pos_ is not "PUNCT"
+            and len(token) > 1
+        )
+    ]
     if len(lemmas) > 10:
         unigram_docs.append(lemmas)
         unigram_datafiles.append(datafile)
@@ -72,10 +77,10 @@ for datafile in data_files:
         filenames.append(datafile)
     counter += 1
     if counter % 1000 == 0:
-        logger.info(f'Tokenised/Lemmatised {counter} of {len(data_files)} files')
+        logger.info(f"Tokenised/Lemmatised {counter} of {len(data_files)} files")
 
 # ========== Add n-grams ==========
-logger.info(f'Generating n-grams and adding them to the corpus')
+logger.info(f"Generating n-grams and adding them to the corpus")
 docs = deepcopy(unigram_docs)  # Make new instance before manipulating docs
 
 # Build bigrams
@@ -83,8 +88,8 @@ bigram = Phrases(docs, min_count=30)
 trigram = Phrases(bigram[docs], min_count=15)
 for i in range(len(docs)):
     doc = docs[i]
-    bigrams_ = [b for b in bigram[doc] if b.count('_') == 1]
-    trigrams_ = [t for t in trigram[bigram[doc]] if t.count('_') == 2]
+    bigrams_ = [b for b in bigram[doc] if b.count("_") == 1]
+    trigrams_ = [t for t in trigram[bigram[doc]] if t.count("_") == 2]
     # print(f'Found bigrams {bigrams_}')
     # print(f'Found trigrams {trigrams_}')
     docs[i] = doc + bigrams_ + trigrams_
@@ -94,23 +99,51 @@ for i in range(len(docs)):
 def vis_ngrams(docs_in, n_ngrams=20):
 
     from collections import Counter
+
     frequencies = Counter([])
     for text in docs_in:
         frequencies += Counter(text)
 
-    unigram_df = pd.DataFrame([{'ngram': k, 'count': v} for k, v in frequencies.items() if '_' not in k]).sort_values('count', ascending=False)
-    bi_gram_df = pd.DataFrame([{'ngram': k, 'count': v} for k, v in frequencies.items() if '_' in k]).sort_values('count', ascending=False)
-    trigram_df = pd.DataFrame([{'ngram': k, 'count': v} for k, v in frequencies.items() if k.count('_') == 2]).sort_values('count', ascending=False)
+    unigram_df = pd.DataFrame(
+        [{"ngram": k, "count": v} for k, v in frequencies.items() if "_" not in k]
+    ).sort_values("count", ascending=False)
+    bi_gram_df = pd.DataFrame(
+        [{"ngram": k, "count": v} for k, v in frequencies.items() if "_" in k]
+    ).sort_values("count", ascending=False)
+    trigram_df = pd.DataFrame(
+        [{"ngram": k, "count": v} for k, v in frequencies.items() if k.count("_") == 2]
+    ).sort_values("count", ascending=False)
     print(unigram_df[:n_ngrams])
     print(bi_gram_df[:n_ngrams])
     print(trigram_df[:n_ngrams])
 
     # Visualise counts of top n-grams
-    fig = px.bar(unigram_df[:n_ngrams], x='ngram', y='count', title='Counts of top unigrams', template='plotly_white', labels={'ngram': 'Unigram', 'count': 'Count'})
+    fig = px.bar(
+        unigram_df[:n_ngrams],
+        x="ngram",
+        y="count",
+        title="Counts of top unigrams",
+        template="plotly_white",
+        labels={"ngram": "Unigram", "count": "Count"},
+    )
     fig.show()
-    fig = px.bar(bi_gram_df[:n_ngrams], x='ngram', y='count', title='Counts of top bi-grams', template='plotly_white', labels={'ngram': 'Bigram', 'count': 'Count'})
+    fig = px.bar(
+        bi_gram_df[:n_ngrams],
+        x="ngram",
+        y="count",
+        title="Counts of top bi-grams",
+        template="plotly_white",
+        labels={"ngram": "Bigram", "count": "Count"},
+    )
     fig.show()
-    fig = px.bar(trigram_df[:n_ngrams], x='ngram', y='count', title='Counts of top trigrams', template='plotly_white', labels={'ngram': 'Trigram', 'count': 'Count'})
+    fig = px.bar(
+        trigram_df[:n_ngrams],
+        x="ngram",
+        y="count",
+        title="Counts of top trigrams",
+        template="plotly_white",
+        labels={"ngram": "Trigram", "count": "Count"},
+    )
     fig.show()
     return True
 
@@ -118,16 +151,32 @@ def vis_ngrams(docs_in, n_ngrams=20):
 # vis_ngrams(docs, n_ngrams=40)  # TODO - THIS IS SLOW AS SHIT
 
 # ========== Further filtering ==========
-logger.info(f'Further cleaning the text by removing custom stopwords & rare/common words')
+logger.info(
+    f"Further cleaning the text by removing custom stopwords & rare/common words"
+)
 # Remove custom stopwords
 custom_stopwords = [
-    'play_important_role', 'play_critical_role', 'play_key_role', '95_confidence_interval',
-    'provide_new_insight',
-    'et_al', 'pubmed_abstract', 'publisher_text', 'present_study', 'results_suggest', 'result_suggest',
-    '95_ci', 'play_important',
-    'study', 'result', 'analysis', 'method'
+    "play_important_role",
+    "play_critical_role",
+    "play_key_role",
+    "95_confidence_interval",
+    "provide_new_insight",
+    "et_al",
+    "pubmed_abstract",
+    "publisher_text",
+    "present_study",
+    "results_suggest",
+    "result_suggest",
+    "95_ci",
+    "play_important",
+    "study",
+    "result",
+    "analysis",
+    "method",
 ]
-docs = [[token for token in doc if token.lower() not in custom_stopwords] for doc in docs]
+docs = [
+    [token for token in doc if token.lower() not in custom_stopwords] for doc in docs
+]
 
 # # Visualis n-grams again
 # vis_ngrams(docs)
@@ -141,11 +190,11 @@ dictionary.filter_extremes(no_below=20, no_above=0.5)
 # Bag-of-words representation of the documents.
 corpus = [dictionary.doc2bow(doc) for doc in docs]
 
-print('Number of unique tokens: %d' % len(dictionary))
-print('Number of documents: %d' % len(corpus))
+print("Number of unique tokens: %d" % len(dictionary))
+print("Number of documents: %d" % len(corpus))
 
 # ========== LDA - train our model with Gensim ==========
-logger.info(f'Training LDA model')
+logger.info(f"Training LDA model")
 # Set training parameters.
 num_topics = 12
 chunksize = 2000
@@ -161,20 +210,20 @@ lda = LdaModel(
     corpus=corpus,
     id2word=id2word,
     chunksize=chunksize,
-    alpha='auto',
-    eta='auto',
+    alpha="auto",
+    eta="auto",
     iterations=iterations,
     num_topics=num_topics,
     passes=passes,
-    eval_every=eval_every
+    eval_every=eval_every,
 )
 
-logger.info(f'Take a look at the results')
-top_topics = lda.top_topics(corpus) #, num_words=20)
+logger.info(f"Take a look at the results")
+top_topics = lda.top_topics(corpus)  # , num_words=20)
 
 # Average topic coherence is the sum of topic coherences of all topics, divided by the number of topics.
 avg_topic_coherence = sum([t[1] for t in top_topics]) / num_topics
-print('Average topic coherence: %.4f.' % avg_topic_coherence)
+print("Average topic coherence: %.4f." % avg_topic_coherence)
 
 # # ========== Visualise the data with pyLDAvis ==========
 # from pprint import pprint
@@ -190,7 +239,9 @@ print('Average topic coherence: %.4f.' % avg_topic_coherence)
 # ========== T-SNE transform ==========
 lda_vals = list()
 for d in corpus:
-    topics_tup = lda.get_document_topics(d)  # This should be a N by K matrix where N = corpus size, K = topics
+    topics_tup = lda.get_document_topics(
+        d
+    )  # This should be a N by K matrix where N = corpus size, K = topics
     temp_dict = {i: 0 for i in range(num_topics)}
     for t in topics_tup:
         temp_dict[t[0]] = t[1]
@@ -199,32 +250,49 @@ for d in corpus:
 lda_df = pd.DataFrame(lda_vals)
 lda_arr = lda_df.values
 
-lda_topics = {i[0]: i[1].split(' + ') for i in lda.print_topics(-1)}
+lda_topics = {i[0]: i[1].split(" + ") for i in lda.print_topics(-1)}
 topics_txt = [lda_topics[i] for i in range(num_topics)]
-topics_txt = [[j.split('*')[1].replace('"', '') for j in i] for i in topics_txt]
-topics_txt = ['; '.join(i) for i in topics_txt]
+topics_txt = [[j.split("*")[1].replace('"', "") for j in i] for i in topics_txt]
+topics_txt = ["; ".join(i) for i in topics_txt]
 
 lda_df = lda_df.assign(topic_id=[str(lda_arr[i].argmax()) for i in range(len(lda_arr))])
-lda_df = lda_df.assign(topic_txt=[topics_txt[lda_arr[i].argmax()] for i in range(len(lda_arr))])
-lda_df = lda_df.assign(topics=['Topic: ' + str(lda_arr[i].argmax()) for i in range(len(lda_arr))])
+lda_df = lda_df.assign(
+    topic_txt=[topics_txt[lda_arr[i].argmax()] for i in range(len(lda_arr))]
+)
+lda_df = lda_df.assign(
+    topics=["Topic: " + str(lda_arr[i].argmax()) for i in range(len(lda_arr))]
+)
 lda_df = lda_df.assign(title=docs_titles)
 lda_df = lda_df.assign(filename=filenames)
 
 # for tsne_perp in [20, 35, 50, 100, 200]:  # Test out different perplexity values
 for tsne_perp in [40]:  # Test out different perplexity values
     tsne_embeds = TSNE(
-        n_components=2, perplexity=tsne_perp, n_iter=350, n_iter_without_progress=100, learning_rate=500, random_state=42,
+        n_components=2,
+        perplexity=tsne_perp,
+        n_iter=350,
+        n_iter_without_progress=100,
+        learning_rate=500,
+        random_state=42,
     ).fit_transform(lda_arr)
-    lda_df = pd.concat([lda_df, pd.DataFrame(tsne_embeds, columns=['x', 'y'])], axis=1)
+    lda_df = pd.concat([lda_df, pd.DataFrame(tsne_embeds, columns=["x", "y"])], axis=1)
 
     # Visualise the t-SNE topics
-    topic_ids = 'Topic: ' + lda_df['topic_id'].astype(str).values
-    fig = px.scatter(lda_df, title='t-SNE test, perplexity: ' + str(tsne_perp),
-                     x='x', y='y', color=topic_ids, color_discrete_sequence=px.colors.qualitative.Light24,
-                     hover_name='title', hover_data=['topic_txt'], template='plotly_white')
+    topic_ids = "Topic: " + lda_df["topic_id"].astype(str).values
+    fig = px.scatter(
+        lda_df,
+        title="t-SNE test, perplexity: " + str(tsne_perp),
+        x="x",
+        y="y",
+        color=topic_ids,
+        color_discrete_sequence=px.colors.qualitative.Light24,
+        hover_name="title",
+        hover_data=["topic_txt"],
+        template="plotly_white",
+    )
     fig.show()
 
-lda.save('outputs/lda_model')
-lda_df.to_csv('outputs/lda_df.csv')
-with open('outputs/lda_topics.json', 'w') as f:
+lda.save("outputs/lda_model")
+lda_df.to_csv("outputs/lda_df.csv")
+with open("outputs/lda_topics.json", "w") as f:
     json.dump(lda_topics, f)
